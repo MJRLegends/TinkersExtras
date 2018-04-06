67ba55a3f03580f8da4cd7733b69f7ef364579e2
MJRLegends <MJRLegends@users.noreply.github.com>
2018-04-06 23:38:35 +0100

    Released v1.0.3

--- src/main/java/com/mjr/tinkersextras/TinkersExtras.java
+++ src/main/java/com/mjr/tinkersextras/TinkersExtras.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = TinkersExtras.MODID, version = TinkersExtras.VERSION, dependencies = "before:tconstruct")
 public class TinkersExtras {
 	public static final String MODID = "tinkersextras";
-	public static final String VERSION = "1.12.2-1.0.2";
+	public static final String VERSION = "1.12.2-1.0.3";
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
