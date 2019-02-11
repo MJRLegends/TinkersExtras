f76598919ca3520ba2073843a1878db69345d6d1
MJRLegends <MJRLegends@users.noreply.github.com>
2019-02-11 19:44:28 +0000

    Released v1.0.7

--- src/main/java/com/mjr/tinkersextras/TinkersExtras.java
+++ src/main/java/com/mjr/tinkersextras/TinkersExtras.java
@@ -1,5 +1,6 @@
 package com.mjr.tinkersextras;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.common.MinecraftForge;
@@ -11,12 +12,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = TinkersExtras.MODID, version = TinkersExtras.VERSION, dependencies = "before:tconstruct", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class TinkersExtras {
 	public static final String MODID = "tinkersextras";
-	public static final String VERSION = "1.12.2-1.0.6";
-	public static Logger logger;
+	public static final String VERSION = "1.12.2-1.0.7";
+	public static Logger logger = LogManager.getLogger();
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		logger = event.getModLog();
 		Config.load();
 		MinecraftForge.EVENT_BUS.register(new EventHandlerMain());
 		Config.load();
