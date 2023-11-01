70552dc576661148c22cc71b56854f5a4312bed7
MJRLegends <MJRLegends@users.noreply.github.com>
2023-11-01 23:14:09 +0000

    Released v1.1.1

--- src/main/java/com/mjr/tinkersextras/TinkersExtras.java
+++ src/main/java/com/mjr/tinkersextras/TinkersExtras.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = TinkersExtras.MODID, version = TinkersExtras.VERSION, dependencies = "before:tconstruct", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class TinkersExtras {
 	public static final String MODID = "tinkersextras";
-	public static final String VERSION = "1.12.2-1.1.0";
+	public static final String VERSION = "1.12.2-1.1.1";
 	public static Logger logger = LogManager.getLogger();
 
 	@Mod.EventHandler
